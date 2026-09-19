9799262c809d7e631b1658bf6a87e8d125304276
Julvy Aditya <julvyaditya@gmail.com>
2026-09-19 23:19:34 +0700

    Add Firebase Analytics to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC89-C22hX3JCnyfk2IEh-wBnxR_-DepE",
+  apiKey: "AIzaSyC89-C22XhM3JCnyfk2IEh-wBnxR_-DepE",
   authDomain: "daily-3d199.firebaseapp.com",
   projectId: "daily-3d199",
   storageBucket: "daily-3d199.firebasestorage.app",
   messagingSenderId: "42117614817",
-  appId: "1:42117614817:web:90228fde2383fcd618bba0"
+  appId: "1:42117614817:web:90228fde2383fcd618bba0",
+  measurementId: "G-THGYKQSBFG"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
